8ecb31a6a4fc64a0ae735b84468e39ad43c891ea
codebender <lionelmbankeu@gmail.com>
2021-09-30 00:05:53 +0200

    finally support for routes next we going to work on the design

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,18 +1,43 @@
-import AppLayout  from './_app';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import AppLayout from "./_app";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+const App = ({ children }) => {
+  return (
+    <AppLayout>
+      <Router>
 
-const App = ({children}) => {
-  return(
-    <AppLayout>{children}</AppLayout>
-  )
+        <nav>
+          <ul>
+          {children.map((i, k) => {
+          if(i.props){
 
-}
- 
+            return(
+              <li>
+              <Link to={i.props.name}>{i.props.name}</Link>
+            </li>
+            )
+  
+          }
+         
+        })}
+          </ul>
+   
+        </nav>
+   
 
-export default App
+        <Switch>
+          {children.map((i, k) => {
+
+            if(i.props.name){
+              return (<Route key={i.props.name} path={`/${i.props.name}`}>{children[k]}</Route>);
+            }
+
+          })}
+        </Switch>
+        
+      </Router>
+    </AppLayout>
+  );
+};
+
+export default App;
